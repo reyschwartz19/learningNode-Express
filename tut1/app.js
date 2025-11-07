59f03835184c2f9dcd0888c92fb154b2a936210e
rey <njongremy30@gmail.com>
2025-11-07 12:55:05 +0100

    modules

---
+++ tut1/app.js
@@ -0,0 +1,7 @@
+const names = require('./names');
+const sayHi = require('./utils');
+const data = require('./alt')
+console.log(data);
+sayHi(names.john);
+sayHi('Susan');
+sayHi(names.peter);
---
+++ tut1/names.js
@@ -0,0 +1,7 @@
+//local
+const secret = 'SUPER SECRET';
+//shared
+const john = 'john';
+const peter = 'Peter';
+
+module.exports = {john, peter};
